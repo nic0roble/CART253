b4e39b8d0852c3b6349dae3e6cbfd089c927b732
nic0roble <n_robled@live.concordia.ca>
2026-09-23 02:33:42 -0500

    I add on my drawing a head and ears

--- prototypes/instructions/prototype.1.representational/js/script.js
+++ prototypes/instructions/prototype.1.representational/js/script.js
@@ -1,24 +1,70 @@
 /**
- * Title of Project
- * Author Name
+ * 3 drawings
+ * Nicolas Robledo S.
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * This is my 1st drawing 
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ 
 */
 function setup() {
+
+createCanvas(500,500);
+background(160,206,222);
+
+//save the current change
+push();
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ 
 */
 function draw() {
 
+//call the function - run the code
+Head();
+Ears();
+
+}
+// Base shape of the fox's head
+ 
+function Head() {
+
+
+    push();
+    fill(237, 145, 33); // 
+    stroke(237, 145, 33);
+    ellipse(250, 280, 220, 200);
+}
+
+// Fox ears
+
+function Ears() {
+
+push();
+    // left ear ext
+    fill(237, 145, 33);
+    stroke(237, 145, 33);
+    triangle(160, 220, 195, 110, 235, 220);
+
+    // left ear int
+    fill(255, 255, 255);
+    stroke(255, 255, 255);
+    triangle(178, 205, 200, 140, 222, 205);
+
+    // right ear ext
+    fill(237, 145, 33);
+    stroke(237, 145, 33);
+    triangle(265, 220, 305, 110, 340, 220);
+
+    // right ear int
+    fill(255, 255, 255);
+    stroke(3255, 255, 255);
+    triangle(278, 205, 300, 140, 322, 205);
+    pop();
 }

f456af065351550c263fbc9e4809097129823028
nic0roble <n_robled@live.concordia.ca>
2026-09-23 15:08:04 -0500

    i created clouds and use the  random() function to create more clouds by clicking

--- prototypes/instructions/prototype.3.weird/js/script.js
+++ prototypes/instructions/prototype.3.weird/js/script.js
@@ -9,26 +9,68 @@
 
 /**
 */
+
+let cloudX;
+let cloudY;
+let cloudSize;
+
+let cloud1X;
+let cloud1Y;
+let cloud1Size;
+
+let cloud2X;
+let cloud2Y;
+let cloud2Size;
+
 function setup() {
 
 createCanvas(500, 750);
 background(28, 52, 84);   
+setCloudPosition();
+
 }
 
 
+}
 /**
 */
 function draw() {
+
+//call the function - run the code
+Clouds();
 
 //moon
  push();
  fill(240, 240, 210);
  stroke(240, 240, 210);
-  circle(380, 120, 200);
+  circle(410, 100, 190);
 
   //moons cut
 fill(28, 52, 84);
 stroke(28, 52, 84);
-circle(410, 100, 200);
+circle(430, 90, 200);
     pop();
+}
+
+function Clouds(){
+push();
+
+  fill(255, 255, 255, 150);
+  stroke(255, 255, 255);
+
+ //Cloud 1
+ellipse(cloudX, cloudY, 90, 30);
+
+ // Cloud 2
+ellipse(cloud1X, cloud1Y, 100, 60);
+
+ // Cloud 3
+ellipse(cloud2X, cloud2Y, 110, 90);
+
+
+
+}
+
+function mousePressed() {
+    setCloudPosition();
 }
